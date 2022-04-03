115feec542a0dddfc4e6cb08715d772176cd413d
willianMachado <wasmachado@gmail.com>
2022-04-03 19:02:27 -0300

    vercel test

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/VueTyper.client.js'
+    'plugins/VueTyper.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
