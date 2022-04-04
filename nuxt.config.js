279c039b9eb69a85865d3d1c31e1dae853189a0d
pedroguibueno <pedroguibueno@bol.com.br>
2022-04-03 22:23:17 -0300

    add font

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,11 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap'
+      }
     ],
     script: [
       {
